273fcd99838e2fdd58d6e9d3c77d7d6f53c78ee2
xinruoyusixian <xinruoyusixian@163.com>
2020-03-30 18:41:41 +0800

    micropython  websocket 链接库
    micropython  websocket 链接库

--- websocket.py
+++ websocket.py
@@ -86,6 +86,7 @@ class websocket:
             res = ''.join(newdata)
             return res
             
+            
 if __name__ == "__main__":
   sock = socket.socket()
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
