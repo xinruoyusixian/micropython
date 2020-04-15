43d3ee78b553d601e4f6e6540978fde324661b96
xinruoyusixian <xinruoyusixian@163.com>
2020-04-15 19:02:58 +0800

    封封装成了类 可以随意调用

--- oled.py
+++ oled.py
@@ -1,20 +1,9 @@
-import time,font
 
-from machine import Pin, I2C
-from time import sleep
-num=font.num
-ch=font.ch
-font=font.font
-i2c = I2C(scl=Pin(4), sda=Pin(5))
-# LCD Control constants
-ADDR = 0x3C
-# Holds the segments on in each of the four digits
-def command(c):
-    i2c.writeto(ADDR, b'\x00' + bytearray(c))
-
-def initialize():
-
-    cmd = [
+class oled:
+    def __init__(self,i2c):
+        self.i2c = i2c
+        self.ADDR = 0x3C
+        cmd = [
         [0xAE],                    # DISPLAYOFF
         [0xA4],           #显示恢复
         [0xD5, 0xF0],            # 设置显示时钟SETDISPLAYCLOCKDIV
@@ -33,57 +22,65 @@ def initialize():
         [0xDB,  0x40],                 # SETVCOMDETECT
         [0xA6],                 # NORMALDISPLAY
         [0xd6, 0],  # zoom off
-
-    ]
-    for c in cmd:
-        command(c)
-    clear()
-    command([0xaf])  # SSD1306_DISPLAYON
-def set(rect):
-  #设置范围 rect[0],rect[1] 从列到几列最大127
-  #         rect[2],rect[3] 从行到几行最大7
-    command([0x21, rect[0], rect[1]]) 
-    command([0x22, rect[2], rect[3]])
-
-def clear(rect=[0, 127, 0, 7]):
-    set(rect)
-    screen = bytearray(33)
-    screen[0] = 0x40
-    print (screen)
-    for i in range(0, 32):
-        write(screen)
+        ]
+        for c in cmd:
+            self.command(c)
+        self.clear()
+        self.command([0xaf])  # SSD1306_DISPLAYON
+    def set(self,rect):
+      #设置范围 rect[0],rect[1] 从列到几列最大127
+      #         rect[2],rect[3] 从行到几行最大7
+        self.command([0x21, rect[0], rect[1]]) 
+        self.command([0x22, rect[2], rect[3]])
+    def clear(self,rect=[0, 127, 0, 7]):
+        self.set(rect)
+        screen = bytearray(33)
+        screen[0] = 0x40
+        for i in range(0, 32):
+            self.write(screen)
+            
+    def command(self,c):
+        self.i2c.writeto(self.ADDR, b'\x00' + bytearray(c))
+        
+    def write(self,b):
+      self.i2c.writeto(self.ADDR, b)
+      # 一个十六进制 控制每一列向下的8个像素
+      #i2c.writeto(ADDR, b'@0\x00\x00\x00\x000')
         
 
-def write(b):
-  sleep(0.1)
-  i2c.writeto(ADDR, b)
-  # 一个十六进制 控制每一列向下的8个像素
-  #i2c.writeto(ADDR, b'@0\x00\x00\x00\x000')
 
-  
-  
-def  f(s):
-  while 1:
-    write(s)
-b=  bytearray(33)  
-def  c(s):
-     for i in range(0, 32):
-        b[i]=s    
-    
+    def draw(self,arr,x,y):
 
 
-def draw(arr,x,y):
-    set([x,127,y,7])
-    for i in range(0,len(arr)):
-        write(arr[i])
-        y+=1 
-        set([x,127,y,7])
-
+        self.set([x,127,y,7])
+        print(x,y)
+        for i in range(0,len(arr)):
+            self.write(arr[i])
+            y+=1 
+            self.set([x,127,y,7])
+            print(x,y)
+    def  fill(self,s,b=1000):
+        p=0  
+        while 1:
+          self.write(s)
+          p+=1
+          if p==b:
+            break
+         
+                     
        
            
-
 if __name__ == "__main__":
-  initialize()
+  from machine import Pin, I2C
+  i2c=I2C(scl=Pin(4), sda=Pin(5))
+  import time,font
+  from time import sleep
+  num=font.num
+  ch=font.ch
+  font=font.font
+  d=oled(i2c)
+  draw=d.draw
+  clear=d.clear
   def display(s):
       
       s=str(s)
@@ -102,6 +99,7 @@ if __name__ == "__main__":
       draw(font[10],x,0)
       x+=2
       draw(font[int(s[4:5])],x,0)
+
       x+=20
       draw(font[int(s[5:6])],x,0)
   while 1:
@@ -139,5 +137,3 @@ if __name__ == "__main__":
     display(t)
     
     sleep(1)
-
-
