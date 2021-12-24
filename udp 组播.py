a8a66ec860e40c815d33cef23a1249a041e697d6
xinruoyusixian <xinruoyusixian@163.com>
2021-12-24 20:20:30 +0800

    Update udp 组播.py

--- udp 组播.py
+++ udp 组播.py
@@ -2,7 +2,7 @@
 import socket 
 import time
 dst = ('224.0.0.251', 5353) 
-#dst = ('255.255.255.255', 5353)  
+#dst = ('255.255.255.255', 5353)  #向所有IP发送
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) 
 s.sendto(b'10036',dst)  
